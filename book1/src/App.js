c52ef29c75bd08572b2f84eb624f7ef1b5fcf5d0
DeerForest <zotnlnn@kpu.ac.kr>
2021-03-02 19:41:46 +0900

    componant map practice

--- book1/src/App.js
+++ book1/src/App.js
@@ -1,6 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
+import ListEx from './ListEx'
+
+const data1 = {
+  text : "data1",
+}
+const data2 = {
+  text : "data2",
+}
+const data3 ={
+  text : "data3"
+}
+
+
+const listData=[data1,data2,data3];
 
 class Hello extends React.Component{
   render(){
@@ -35,12 +49,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/*
         <Hello></Hello>
         <Hello2></Hello2>
         <Hello3></Hello3>
         <Hello4></Hello4>
         <Hello5></Hello5>
+        */
+        }
         <Hello6 data={data}></Hello6>
+        <ListEx data={listData}></ListEx>
       </header>
     </div>
   );
